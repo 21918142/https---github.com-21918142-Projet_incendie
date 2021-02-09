bf723d9455da8e8fa6f3943a870669d195d3014a
Zinaida <zinaida.benaoudia@ens.uvsq.fr>
2021-02-09 09:55:44 +0100

    debut

--- incendie.py
+++ incendie.py
@@ -7,10 +7,21 @@
 # https://github.com/21918142/Projet_incendie.git
 #############################################
 
+#############################################
 #import des librairies :
 import as tkinter :
+#############################################
 
+#############################################
 #definition des constantes :
+#LONGUEUR = longueur du rectangle
+#LARGEUR = largeur du rectangle 
+#PARS_EAU = carré d'eau reste (jusqu'a la fin)
+#PARS_FORET = carré foret 
+#PARS_FEU = carré de feu 
+#PARS_PRAIRIE = carré prairie 
+#PARS_CENDRE_TIEDE = carré cendre tiède 
+#PARS_CENDRE_ETEINTE = carré cendre chaude
+#############################################
 
-gcj
-variables globales :
+#variables globales :
