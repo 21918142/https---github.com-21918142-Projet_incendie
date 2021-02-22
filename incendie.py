581c0c78fbd41b9e144a4d2370fb5e13f575cf24
21918142 <kevin.su@ens.uvsq.fr>
2021-02-22 21:51:09 +0100

    tarrain début

--- incendie.py
+++ incendie.py
@@ -1,15 +1,17 @@
-########################################################################
-# groupe MPCI 1
-# Céline DJADEL
+#########################################################################
+# Groupe MPCI 1
+# Celine DJADEL
 # Zinaïda BENAOUDIA
-# Yamcel ABEDESSELAM
+# Yamcel ABDESSLAM
+# Kévin SU
 # Brice AUGUSTIN
 # Lilya LAHJAILY
 # https://github.com/21918142/https---github.com-21918142-Projet_incendie
 #########################################################################
 
 # IMPORT DES LIBRAIRIES :
-from tkinter import*
+import tkinter as tk
+import random
 
 # CONSTANTES :
 LARGEUR = 800
@@ -17,16 +19,24 @@ HAUTEUR = 500
 
 # VARIABLES GLOBALES :
 
+
 # FONCTIONS :
+
+def genere_terrain() :
+    for i in range(0,LARGEUR,10) :
+        for j in range(0,HAUTEUR,10) :
+            canvas.create_rectangle((i,j), (i+10,j+10), fill = "white")
 
 # PROGRAMME :
 # / Fenetre /
-racine = Tk()
-racine.config(width=LARGEUR, height=HAUTEUR)
+racine = tk.Tk()
 racine.title("Simulation de la propagation d'un incendie")
 
+canvas = tk.Canvas(racine, width = LARGEUR , height = HAUTEUR)
+canvas.grid(column = 1, row = 1, columnspan = 1, rowspan = 5)
 
-
+Bouton1 = tk.Button(racine, text = "Génère un terrain", command = genere_terrain)
+Bouton1.grid(column = 0, row = 1)
 
 
 
