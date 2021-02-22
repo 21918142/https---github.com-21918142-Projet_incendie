0f99453507573d869db11e197e49c78ef932bf7d
uvsq22011529 <71547179+uvsq22011529@users.noreply.github.com>
2021-02-22 11:49:14 +0100

    Update incendie.py

--- incendie.py
+++ incendie.py
@@ -45,25 +45,3 @@ racine.title("Simulation de la propagation d'un incendie")
 
 # / Gestion de la fenetre /
 racine.mainloop()
-=======
-###################################################
-
-#import des librairies :
-import as tkinter :
-#############################################
-
-#############################################
-#definition des constantes :
-#LONGUEUR = longueur du rectangle
-#LARGEUR = largeur du rectangle 
-#PARS_EAU = carré d'eau reste (jusqu'a la fin)
-#PARS_FORET = carré foret 
-#PARS_FEU = carré de feu 
-#PARS_PRAIRIE = carré prairie 
-#PARS_CENDRE_TIEDE = carré cendre tiède 
-#PARS_CENDRE_ETEINTE = carré cendre chaude
-#############################################
-
-#variables globales :
-blabla
->>>>>>> 75fa8558442158dd2e9d19e8d5c5126fed1ad83b
